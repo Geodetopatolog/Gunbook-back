09d938ad05d6b533bb89ce6ea4d88187ce72b9d9
Rafał Szatkowski <rafalszatkowski1992@gmail.com>
2022-03-02 19:25:10 +0100

    Initial commit

---
+++ src/main/java/pl/portalstrzelecki/psback/PSbackApplication.java
@@ -0,0 +1,13 @@
+package pl.portalstrzelecki.psback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PSbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PSbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/portalstrzelecki/psback/PSbackApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.portalstrzelecki.psback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PSbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
